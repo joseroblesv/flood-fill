4d0d88f43de2c40a734750a1877e687ddb87b8fc
Jose <joserobles1995@gmail.com>
2020-04-23 22:17:06 +0200

    adding new tests

--- src/main/java/com/dojo/World.java
+++ src/main/java/com/dojo/World.java
@@ -2,15 +2,52 @@ package com.dojo;
 
 public class World {
 
+    private int width;
+    private int height;
     private String grid;
 
-    public World(String grid) {
 
+    public World(int width, int height, String grid) {
+        this.width = width;
+        this.height = height;
         this.grid = grid;
     }
 
     @Override
     public String toString() {
         return grid;
+    }
+
+    public void fill(int x, int y, String character) {
+        String[][] matrix = fillMatrix(character);
+        StringBuilder paintedGrid = paintMatrix(matrix);
+
+        this.grid = paintedGrid.toString();
+    }
+
+    private StringBuilder paintMatrix(String[][] matrix) {
+        StringBuilder paintedGrid = new StringBuilder();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                paintedGrid.append(matrix[i][j]);
+            }
+            paintedGrid.append("\n");
+        }
+        return paintedGrid;
+    }
+
+    private String[][] fillMatrix(String character) {
+        String[][] matrix = new String[this.width][this.height];
+        String[] chars = grid.replaceAll("\n", "").split("");
+        int count = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                matrix[i][j] = chars[count].equals(".") ? character : chars[count];
+                count++;
+            }
+        }
+        return matrix;
     }
 }
--- src/test/java/com/dojo/WorldTest.java
+++ src/test/java/com/dojo/WorldTest.java
@@ -9,22 +9,61 @@ public class WorldTest {
     @Test
     public void returnEmptyString_whenGridIsEmpty() {
         String expectedGrid = "";
-
-        World world = new World(expectedGrid);
+        int expectedWidth = 0;
+        int expectedHeight = 0;
+        World world = new World(expectedWidth, expectedHeight, expectedGrid);
 
         assertEquals(expectedGrid, world.toString());
     }
 
     @Test
-    public void returnWorld_whenGridIsFilled() {
+    public void returnWorld_whenGridIsCreated() {
         String expectedGrid = """
                 XXX
-                X X
+                X.X
                 XXX
                 """;
 
-        World world = new World(expectedGrid);
+        World world = new World(3, 3, expectedGrid);
 
+        assertEquals(expectedGrid, world.toString());
+    }
+
+    @Test
+    public void returnWorld_whenGridIsFilled3x3() {
+        String initialGrid = """
+                XXX
+                X.X
+                XXX
+                """;
+        World world = new World(3, 3, initialGrid);
+        world.fill(1,1,"@");
+
+        String expectedGrid = """
+                XXX
+                X@X
+                XXX
+                """;
+        assertEquals(expectedGrid, world.toString());
+    }
+
+    @Test
+    public void returnWorld_whenGridIsFilled4x4() {
+        String initialGrid = """
+                XXXX
+                X..X
+                XXXX
+                XXXX
+                """;
+        World world = new World(4, 4, initialGrid);
+        world.fill(1,1,"@");
+
+        String expectedGrid = """
+                XXXX
+                X@@X
+                XXXX
+                XXXX
+                """;
         assertEquals(expectedGrid, world.toString());
     }
 }
